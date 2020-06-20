2bf3ed81c1848e9bc5e23f7bae570e0cbe1555b1
yang2113666 <884145202@qq.com>
2020-06-20 16:27:53 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,10 +1,19 @@
-// 每次调用$.get()、$.post()、$.ajax()的时候，
-// 会先调用ajaxPrefikter这个函数
+// 注意：每次调用 .get() 或 .post() 或 $.ajax() 的时候，
+
+// 会先调用 ajaxPrefilter 这个函数
+
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 
 $.ajaxPrefilter(function (options) {
-    // 在发起ajax请求之前，统一拼接请求的根路径
+
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+
     options.url = 'http://ajax.frontend.itheima.net' + options.url
 
-
+    // 统一为有权限的接口，设置header请求头
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
